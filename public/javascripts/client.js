ddeb3c926fe86d21541bdbe53f586aa1a24b457e
Zach <zmcartor@gmail.com>
2014-04-16 23:26:11 -0600

    Correct wrong underscore lib and JS mistakes

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -12,24 +12,41 @@ egghead.scoreRow = Backbone.Model.extend({
 egghead.scoreCollection = Backbone.Collection.extend({
   model: egghead.scoreRow,
   initialize: function() {
-    this.listenTo(this, "add", checkHighScore);
+    this.listenTo(this, "add", this.checkHighScore);
   },
+
   checkHighScore : function(newModel){
     this.sort();
     if(parseInt(newModel.get('score'), 10) > parseInt(this.at(0).get('score'), 10) ){
       egghead.eventBus.trigger("newHighScore", parseInt(newModel.get('score'), 10) );
     }
   },
+
   comparator: function(score) {
     return this.get('score');
   }
+
+});
+
+egghead.scoreRow = Backbone.View.extend({
+  initialize: function(){
+    this.render();
+  },
+
+  render: function(){
+    var template = _.template("template string" , this.model.toJSON());
+    this.$el.html(template());
+  },
+  el: 'tr'
 });
 
 
 $(function(){
+var scoreCollection = new egghead.scoreCollection();
 var socket = io.connect('http://localhost');
 socket.on('newScore', function (data) {
-
-
-
+  scoreCollection.add(data);
+  console.log('added some data!');
+  console.log(scoreCollection);
+});
 });
